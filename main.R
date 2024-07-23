156686e2274759d12102972a33db0ea87243b534
armarsh64 <armarsh64@gmail.com>
2024-07-23 17:25:55 +0100

    Data imported

---
+++ main.R
@@ -0,0 +1,13 @@
+library(tidytuesdayR)
+library(dplyr)
+library(ggplot2)
+library(tidyverse)
+
+tuesdata <- tidytuesdayR::tt_load('2024-07-23')
+
+auditions <- tuesdata$auditions
+eliminations <- tuesdata$eliminations
+finalists <- tuesdata$finalists
+ratings <- tuesdata$ratings
+seasons <- tuesdata$seasons
+songs <- tuesdata$songs
